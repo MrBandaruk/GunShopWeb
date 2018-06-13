b452fcc97f22d36b451bba0bafbe911467ffdb63
Nikita <nbandaruk@gmail.com>
2018-06-13 23:48:33 +0300

    Create about page.

--- GunShopClient/src/app/pages/home/home.component.ts
+++ GunShopClient/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var Rellax: any;
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const rellax = new Rellax('.rellax');
   }
+
+
 
 }
