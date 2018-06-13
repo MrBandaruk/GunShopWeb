93dce15020fce2f896648f4350fd4b11ec2b81ed
MrBandaruk <nbandaruk@gmail.com>
2018-06-13 18:33:12 +0300

    Adding home page.

--- GunShopClient/src/app/app.module.ts
+++ GunShopClient/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import {RouterModule, Routes} from '@angular/router';
 
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
