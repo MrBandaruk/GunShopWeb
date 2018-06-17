514f4789d26a3ed56a6bdbb8a5889c77718e5522
MrBandaruk <nbandaruk@gmail.com>
2018-06-18 00:12:11 +0300

    Finish landing. Adding login page and items page.

--- GunShopClient/src/app/app.module.ts
+++ GunShopClient/src/app/app.module.ts
@@ -6,21 +6,32 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
 import {RouterModule, Routes} from '@angular/router';
+import { ItemsComponent } from './pages/items/items.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {MatButtonModule, MatToolbarModule} from '@angular/material';
+import { LoginComponent } from './pages/login/login.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'items', component: ItemsComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ItemsComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
